a817b866b5e3cfad3a4221f9229cfdac5afaefb3
Riley Peters <rileypeters2016@gmail.com>
2022-12-02 21:27:22 -0500

    Added the blueprints for views and auth\nThe website now displays dummy text

--- website/__init__.py
+++ website/__init__.py
@@ -7,4 +7,12 @@ def create_app():
     # Encrypt/Secure session data
     app.config['SECRET_KEY'] = '2340c78hnm0uh)(MH*FWpppxXPM*(H$F#$F*()J'
     
+    # Import the Blueprints
+    from .views import views
+    from .auth import auth
+    
+    # Register the blueprints with a default prefix of /
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
     return app
